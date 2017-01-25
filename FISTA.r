2a59d933e467fbb6caa7c73f7385e3d23dad1594
CRQ <ruqianchen@gmail.com>
2017-01-25 01:17:36 -0800

    Update FISTA.r

--- FISTA.r
+++ FISTA.r
@@ -1,96 +1,7 @@
-dev.off
-rm(list=ls(all=TRUE))
-library("glmnet")
-setwd("/Users/chenruqian/Desktop/desktop/Academic/WInter\ 2017/STAT538/Syllabus\ and\ Homeworks")
-
-# Input
-# Reference for reading inputs http://www.ats.ucla.edu/stat/r/modules/raw_data.htm
-myData <- read.table("crime.txt", header = FALSE)
-
-# Pre-processing
-# Reference for standardization http://stackoverflow.com/questions/15215457/standardize-data-columns-in-r
-df <- myData[,-2]
-df[,-1] <- scale(df[,-1])
-df[,1] <- df[,1] - mean(df[,1])
-# check that we get mean of 0 and sd of 1 for V2, ..., V7 and just mean 0 for V1
-colMeans(df) # faster version of apply(scaled.dat, 2, mean)
-apply(df, 2, sd)
-N <- dim(df)[1] # number of sample points
-
-# Utility functions
-L1_norm<- function(x) sum(abs(x)) / length(x)
-
-#################
-### Use Package #
-###  GLMNET   ###
-#################
-# Manual https://cran.r-project.org/web/packages/glmnet/glmnet.pdf
-# Coercing X into matrix http://stackoverflow.com/questions/30717201/glmnet-not-training-am-i-using-it-wrong
-count3 <- 1
-lambdaCV <- c()
-while(count3 < 5){
-  modelCV<- cv.glmnet(as.matrix(df[,-1]), df[,1])
-  lambdaCV <- append(lambdaCV, modelCV$lambda.min)
-  count3 <- count3 + 1
-} 
-lambdaCV <- mean(lambdaCV)
-
-#################
-betaInfo <- data.frame(matrix(vector(), 1, 5),stringsAsFactors=F)
-# fix lambdaCV at the obtained value 34.2453
-counter <- 1
-seq <- seq(from = 0.5*lambdaCV, to = 1.5*lambdaCV, by = 0.2)
-while(counter < 100){
-  # bootstrapping, selecting with replacement
-  bag <- floor(runif(100, 1, 51)) # each bag has 100 observations
-  model<- glmnet(as.matrix(df[bag,-1]), df[bag,1], lambda = rev(seq))
-  beta <- as.vector(model$beta[,ceiling(length(seq)/2)])
-  betaInfo <- rbind(betaInfo, beta)
-  counter <- counter + 1
-} 
-betaInfo <- betaInfo[-1,]
-
-standardError <- c()
-counter2 <- 1
-while(counter2 < 6){
-  sem<-sd(betaInfo[,counter2])/sqrt(length(betaInfo[,counter2]))
-  standardError <- append(standardError,sem)
-  counter2 <- counter2+1
-}
-plot(standardError)
-
-
-
-#################
-betaInfo <- data.frame(matrix(vector(), 1, 6),stringsAsFactors=F)
-# fix lambdaCV at the obtained value 34.2453
-counter <- 1
-lambdaCVvector <- c()
-while(counter < 100){
-  # bootstrapping, selecting with replacement
-  bag <- floor(runif(100, 1, 51)) # each bag has 100 observations
-  modelCVtmp<- cv.glmnet(as.matrix(df[bag,-1]), df[bag,1])
-  lambdaCVtmp <- modelCVtmp$lambda.min
-  lambdaCVvector <- append(lambdaCVvector, lambdaCVtmp)
-  seq <- seq(from = 0.5*lambdaCVtmp, to = 1.5*lambdaCVtmp, by = 0.2)
-  model<- glmnet(as.matrix(df[bag,-1]), df[bag,1], lambda = rev(seq))
-  beta <- as.vector(model$beta[,ceiling(length(seq))])
-  betaInfo <- rbind(betaInfo, beta)
-  counter <- counter + 1
-} 
-betaInfo <- betaInfo[-1,]
-
-standardError <- c()
-counter2 <- 1
-while(counter2 < 6){
-  sem<-sd(betaInfo[,counter2])/sqrt(length(betaInfo[,counter2]))
-  standardError <- append(standardError,sem)
-  counter2 <- counter2+1
-}
-plot(standardError)
-
-
-#############################################################################
+# This file contains ridge regression: gradient descent + accelerated gradient descent (FISTA)
+#######################################################################################
+# Auxiliary - inputs, functions, 
+#######################################################################################
 dev.off
 rm(list=ls(all=TRUE))
 library("glmnet")
@@ -98,66 +9,161 @@ setwd("/Users/chenruqian/Desktop/desktop/Academic/WInter\ 2017/STAT538/Syllabus\
 # Input
 # Reference for reading inputs http://www.ats.ucla.edu/stat/r/modules/raw_data.htm
 myData <- read.table("crime.txt", header = FALSE)
-# Pre-processing
-# Reference for standardization http://stackoverflow.com/questions/15215457/standardize-data-columns-in-r
-df <- myData[,-2]
-df[,-1] <- scale(df[,-1])
-df[,1] <- df[,1] - mean(df[,1])
 
-# Define X, Y matrices 
-# X <- as.matrix(myData[,-c(1,2)])
-# Y <- as.matrix(myData[,1])
-X <- as.matrix(df[,-1])
-Y <- as.matrix(df[,1])
-N <- dim(X)[1] # number of sample points 
-beta <- as.matrix(c(1,2,3,4,5))
-lambdaMax <- max(1/50 * abs(t(X)%*%Y)) # = 153.59
+X <- as.matrix(myData[,3:7])
+Y <- as.matrix(myData[,1])
 
-softThres <- function(x, lambda) {
-  if (x >= lambda) x - lambda
-  else if (x<lambda & x > -lambda) 0
-  else  lambda + x
+# starting point
+fistaY <- 0
+fistaLambda <- 0
+
+# I used glmnet and built-in cross-validation  to find a good lambda.
+# Alternatively lambda can be found using codes for exe3.
+modelCV <- cv.glmnet(X, Y, alpha = 0) # alpha=0 represents ridge cost
+lambda <- modelCV$lambda.min
+
+# The ridge regression cost function, in penalty form. We want to minimize this over the space of beta. 
+f <- function(beta) t(Y- X %*% beta) %*% (Y- X %*% beta) + lambda * t(beta) %*% beta
+# The gradient wrt beta is grad_beta = ((Y-X beta)^tr (Y-X beta) + lambda beta^tr beta)  = -2* X^tr (Y-X beta) + 2 * lambda * beta
+grad  <- function(beta) -2*t(X) %*% (Y-X %*% beta) + 2*lambda* beta
+
+l2norm<- function(x) sqrt(sum(x^2))
+
+# we can find the least squares beta using Part 1 as well. Just set lambda = 0 and run f, grad again. Then run part 1.
+#lambda <- 0
+#f <- function(beta) t(Y- X %*% beta) %*% (Y- X %*% beta) + lambda * t(beta) %*% beta
+#grad  <- function(beta) -2*t(X) %*% (Y-X %*% beta) + 2*lambda* beta
+betaLS <- c(11.2049468, -0.1557923, 13.7888007, 3.6585927, -1.4853808)
+betaLSl2norm <- l2norm(betaLS)
+
+#######################################################################################
+# Part 1: gradient descent (with armijo to choose step size)
+#######################################################################################
+beta <- rep(0,5)
+t <- 1
+valueArray <- c()
+betaTraj <- beta
+descentStepNum <- 90
+while(t<descentStepNum){
+  t <- t+1
+  # The Armijo backtracking step to find the appropriate step size
+  alpha <- 0.5
+  gamma <- 0.8
+  s <- 1 # step size = 1/L, starting at L=1, i.e. s=1
+  delta <- function(beta) (-1) * grad(beta)
+  # while loop fixing beta
+  fbeta <- f(beta)
+  gradel <- t(grad(beta)) %*% delta(beta)
+  sArray <- c()
+  counter <- 1
+  line1 <- c()
+  line2 <- c()
+  # Below I limited the number of iterations to 100. Even without limiting this, 
+  # we experimentally see that generally we need to update s around 60 times.
+  # I added an upper bound here for safety, avoid being stuck in a while loop.
+  while (f(beta + s*delta(beta)) > fbeta + alpha * s * gradel & counter < 100){
+    line1 <- append(line1, f(beta + s*delta(beta)))
+    line2 <- append(line2, fbeta + alpha * s * gradel)
+    counter <- counter + 1
+    sArray <- append(sArray, s)
+    s <- gamma * s 
+  }
+  
+  # plot(sArray, main = paste("Armijo tries step size, when t =", t), xlab = "counter", ylab = "s") 
+  # In the above plot, we see that the step size decreases as a geometric series (gamma)^n, as defined in the algorithm.
+  
+  graph <- data.frame(line1, line2)
+  matplot(graph,type=c("o","o"), pch=c(1,2),col=c("purple", "red"))
+  legend("topright", inset=.05, legend=c("f(beta + s*delta(beta))", "fbeta + alpha * s * gradel"), pch=c(1,2), col=c("purple","red"), horiz=TRUE)
+  title(paste("Armijo t =", t))
+  
+  beta <- beta - s * grad(beta)
+  betaTraj <- rbind(betaTraj, t(beta))
+  valueArray <- append(valueArray, f(beta))
 }
+plot(valueArray, main = "ridge cost function", xlab = "step# t", ylab = "cost")
+# In the above plot, ideally I see that the cost decreases strictly.
 
-#vecR <- function(beta, j)  Y-(X[, -j] %*% beta[-j]) # a vector of length N 
-#betaUpdate <- function(r, lambda,N,j) {1/N * (X[,j]%*%r), lambda)}
-
-vecR <- function(beta)  Y-(X %*% beta)
-betaUpdate <- function(r, lambda,N,j,expre) {if(expre) softThres( (beta[j] + (1/N) * (X[,j]%*%r)), lambda) else beta[j]}
-
-beta <- as.matrix(c(0,0,0,0,0))
-lambda <-25
-
-# 154
-# 152~100: first coord nonzero
-# 50: 1, 3, 4
+times <- c(0:365)
+t1 <- seq(0,365)
+plot(times, t1, type="l")
 
 
-#vecR1 <- vecR(beta, 1)
-#vecR2 <- vecR(beta, 2)
-#vecR3 <- vecR(beta, 3)
-#vecR4 <- vecR(beta, 4)
-#vecR5 <- vecR(beta, 5)
+xAxis <- (apply(betaTraj, 1, l2norm) /betaLSl2norm)[-1]
+betaGraph <- data.frame(betaTraj[,1], betaTraj[,2],betaTraj[,3],betaTraj[,4],betaTraj[,5]) [-1,]
+matplot(xAxis,betaGraph,type=rep("o",5), pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"))
+legend("topleft", inset=.05,  legend=c("funding","hs","not-hs","college","college4"),cex = 0.5, pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"), horiz=TRUE)
+title(paste("Ridge Regression with num of descents made = ",descentStepNum))
 
-#randomTF <- sample(c(TRUE,FALSE), 1, TRUE)
-randInd <- floor(runif(1,1,6))
-randomTF <- c(FALSE, FALSE, FALSE, FALSE, FALSE)
-randomTF[randInd] <- TRUE
 
-vecRnew <- vecR(beta)
 
-betaUpdate1 <- betaUpdate(vecRnew, lambda,N, 1, randomTF[1])
-betaUpdate2 <- betaUpdate(vecRnew, lambda,N, 2, randomTF[2])
-betaUpdate3 <- betaUpdate(vecRnew, lambda,N, 3, randomTF[3])
-betaUpdate4 <- betaUpdate(vecRnew, lambda,N, 4, randomTF[4])
-betaUpdate5 <- betaUpdate(vecRnew, lambda,N, 5, randomTF[5])
-beta <- cbind(betaUpdate1,betaUpdate2,betaUpdate3,betaUpdate4,betaUpdate5)
+#######################################################################################
+# FISTA Accelerated gradient descent (with armijo to choose step size)
+#######################################################################################
 
-# path wise gradient descent, using previous lambda as warm start
-
-################# scratch ################
-softThres2 <- function(x) {
-  if (x >= 2) x - 2
-  else if (x<2 & x > -2) 0
-  else  2 + x
+beta <- rep(0,5)
+t <- 1
+valueArray <- c()
+betaTraj <- beta
+descentStepNum <- 90
+while(t<descentStepNum){
+  t <- t+1
+  
+  alpha <- 0.5
+  gamma <- 0.8
+  s <- 1 # step size = 1/L, starting at L=1, i.e. s=1
+  delta <- function(beta) (-1) * grad(beta)
+  # while loop fixing beta
+  fbeta <- f(beta)
+  gradel <- t(grad(beta)) %*% delta(beta)
+  counter <- 1
+  sArray <- c()
+  while (f(beta + s*delta(beta)) > fbeta + alpha * s * gradel){
+    counter <- counter + 1
+    sArray <- append(sArray, s)
+    s <- gamma * s 
+  }
+  plot(sArray, main = paste("t =", t), xlab = "counter", ylab = "s") # I want to see the step sizes decreasing
+  
+  betaNew <- fistaY - s * grad(beta) # beta_(t+1)
+  fistaLambdaNew <- 0.5 * (1+ (1+4*fistaLambda^2)^0.5) # lambda_(t+1)
+  fistaY <- betaNew + (fistaLambda-1)/fistaLambdaNew * (betaNew - beta) # y_(t+1)
+  beta <- betaNew 
+  fistaLambda <- fistaLambdaNew
+  
+  valueArray <- append(valueArray, f(beta))
+  betaTraj <- rbind(betaTraj, t(beta))
 }
+plot(valueArray, main = "accelerated ridge cost function", xlab = "step# t", ylab = "cost")
+# In this plot, note that FISTA cost does not decrease strictly, but instead have tiny bumps while decreasing.
+
+xAxis <- (apply(betaTraj, 1, l2norm) /betaLSl2norm)[-1]
+betaGraph <- data.frame(betaTraj[,1], betaTraj[,2],betaTraj[,3],betaTraj[,4],betaTraj[,5]) [-1,]
+matplot(xAxis,betaGraph,type=rep("o",5), pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"))
+legend("bottomright", inset=.05,  legend=c("funding","hs","not-hs","college","college4"),cex = 0.5, pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"), horiz=TRUE)
+title(paste("Ridge Regression FISTA with num of descents made = ",descentStepNum))
+
+matplot(betaGraph,type=rep("o",5), main =paste("Ridge Regression FISTA with num of descents made = ",descentStepNum), pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"))
+legend("bottomright", inset=.05,  legend=c("funding","hs","not-hs","college","college4"),cex = 0.5, pch=c(1,2,3,4,5),col=c("purple", "red","black","blue","green"), horiz=TRUE)
+
+
+#######################################################################################
+# Armijo (backup version)
+#######################################################################################
+
+alpha <- 0.5
+gamma <- 0.8
+s <- 1 # step size = 1/L, starting at L=1, i.e. s=1
+delta <- function(beta) (-1) * grad(beta)
+# while loop fixing beta
+fbeta <- f(beta)
+gradel <- t(grad(beta)) %*% delta(beta)
+counter <- 1
+sArray <- c()
+while (f(beta + s*delta(beta)) > fbeta + alpha * s * gradel){
+  counter <- counter + 1
+  sArray <- append(sArray, s)
+  s <- gamma * s 
+}
+plot(sArray, main = paste("t =", t), xlab = "counter", ylab = "s") # I want to see the step sizes decreasing
+
